d0b196e0651ddcd5260dee7c0156db6610316257
Jared DiScipio <36304698+p0pd0c@users.noreply.github.com>
2021-09-18 22:02:36 -0400

    add filter timestamp type

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,5 +1,8 @@
+import React from 'react'
 export default function Filter({ filter, setFilter, handleFilterSubmit, criteria, setCriteria }) {
-
+    React.useEffect(() => {
+        setCriteria({ ...criteria, from: "", to: "" })
+    }, [])
     return (
         <div id="filterContainer" className="flex flex-row w-full p-1">
             <label className="text-white p-1 w-1/4">Search By</label>
@@ -10,12 +13,25 @@ export default function Filter({ filter, setFilter, handleFilterSubmit, criteria
               <option value="black">black</option>
               <option value="details">detail</option>
               <option value="gameID">gameID</option>
+              <option value="timestamp">timestamp</option>
             </select>
-            { filter !== "" &&
+            { (filter !== "" && filter !== "timestamp") &&
               <form onSubmit={handleFilterSubmit} className="w-1/2">
                 <input type="text" name="criteria" placeholder="search criteria" value={criteria} onChange={(e) => setCriteria(e.target.value)} className="w-full focus:outline-none p-1" />
                 <button type="submit" className="text-white text-center hover:bg-white hover:text-black focus:outline-none w-full p-1">Search</button>
               </form>
+            }
+            { (filter === "timestamp" && criteria.from && criteria.to) &&
+                <form onSubmit={handleFilterSubmit}>
+                    <div className="flex flex-row">
+                      <div className="flex flex-column w-1/2">
+                        <input type="datetime-local" placeholder="From" value={criteria.from} onChange={(e) => setCriteria({ ...criteria, from: e.target.value })} className="h-1/2 focus:outline-none p-1" />
+                        <input type="datetime-local" placeholder="To" value={criteria.to} onChange={(e) => setCriteria({ ...criteria, to: e.target.value })} className="h-1/2 focus:outline-none p-1" />
+                      </div>
+                      <button type="submit" className="text-white text-center hover:bg-white hover:text-black focus:outline-none w-1/2 p-1">Search</button>
+                    </div>
+                </form>
+            
             }
             { filter === "" &&
                 <button className="text-white w-1/2 focus:outline-none hover:bg-white hover:text-black" onClick={handleFilterSubmit}>Reset</button>
